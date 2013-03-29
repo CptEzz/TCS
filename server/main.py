ba851e8638c639de7e034e9c13f0739548f87217
Benny Mackney <bmackneypublic@gmail.com>
2013-03-29 22:00:20 +1100

    ssl works, certificates included are temporary

--- server/main.py
+++ server/main.py
@@ -4,7 +4,10 @@ import tcsapp
 if __name__=="__main__":
     config = {
         'server.socket_host':'0.0.0.0',
-        'server.socket_port':8080
+        'server.socket_port':8080,
+        'server.ssl_module':'builtin',
+        'server.ssl_certificate':'server.crt',
+        'server.ssl_private_key':'server.key'
     }
     cherrypy.config.update(config)
     cherrypy.quickstart(tcsapp.Root())
