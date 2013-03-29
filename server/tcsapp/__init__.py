8ba37245adc8c60479139a895849a877d9a3670e
Benny Mackney <bmackneypublic@gmail.com>
2013-03-29 22:54:59 +1100

    example of session cookies

--- server/tcsapp/__init__.py
+++ server/tcsapp/__init__.py
@@ -1,9 +1,14 @@
 import security
 import cherrypy
+import time
 
 class Root(object):
     @cherrypy.expose
     def index(self):
-        returnData = "Hello World!\n"
+        returnData = "Hello World!\n"        
         returnData += security.passwd(returnData,"salt")
+        if 'last' in cherrypy.request.cookie:
+            last = cherrypy.request.cookie
+            returnData +="\nYou were last here at %s" % (last)
+        cherrypy.response.cookie['last'] = str(time.time())
         return returnData
