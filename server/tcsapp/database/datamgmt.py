7dd9fed6b1f4bbc5a0b40a3c6d23bb72876b8d53
Lachlan Healey <lachlan.j.healey@gmail.com>
2013-04-02 18:56:10 +1000

    Added README file, aswell as __init__.py and temporary database file.

--- server/tcsapp/database/datamgmt.py
+++ server/tcsapp/database/datamgmt.py
@@ -1,8 +1,13 @@
 import sqlite3
 import os
-import security
+#import tcsapp.test
+from .. import security
 
-con = sqlite3.connect('data.sqlite')
+
+
+
+database = 'data.sqlite'
+con = sqlite3.connect(database)
 cur = con.cursor()
 
 
@@ -11,19 +16,21 @@ def init():
     cur.execute('create table student(ID, name, email, password, salt)')
     cur.execute('create table class(ID, code, name, teacher)')
     cur.execute('create table enrollment(studentID, classID, period, day, room)')
+    print 'Initialised'
 
 def register(name, email, password):
     salt = os.urandom(30)
-    cur.execute("insert into student values (?, ?, ?, ?, ?)", (ID, name, email, passwrd(password, salt), salt))
+    cur.execute('insert into student values (?, ?, ?, ?, ?)', (ID, name, email, passwd(password, salt), salt))
     
 
 
 
 def createClass(code, name, teacher):
-    cur.execute("insert into class values (?, ?, ?, ?, ?)", (ID, code, name, teacher))
+    cur.execute('insert into class values (?, ?, ?, ?, ?)', (ID, code, name, teacher))
 
 
 
 def enrol(studentID, classID, period, day, room):
-    return
+    cur.execute('insert into enrollment values (?, ?, ?, ?, ?)', (studentID, classID, period, day, room))
+
 
