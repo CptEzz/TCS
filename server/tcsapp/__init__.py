b96a2ad7e35a3502fde933298f35323aacd838d5
Benny Mackney <bmackneypublic@gmail.com>
2013-04-02 19:40:36 +1100

    added /jsontest

--- server/tcsapp/__init__.py
+++ server/tcsapp/__init__.py
@@ -1,6 +1,7 @@
 import security
 import cherrypy
 import time
+import json
 
 class Root(object):
     @cherrypy.expose
@@ -12,3 +13,9 @@ class Root(object):
             returnData +="\nYou were last here at %s" % (last)
         cherrypy.response.cookie['last'] = str(time.time())
         return returnData
+        
+    @cherrypy.expose
+    def jsontest(self):
+        data = {'message':'Hello JSON world',
+                'batman':'http://i.imgur.com/G11Bocg.gif'}
+        return json.dumps(data)
