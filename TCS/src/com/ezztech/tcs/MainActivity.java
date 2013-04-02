407757688d797376a00a72a2e0d9831121a0a9bd
Ezzy <Ezzy@192.168.0.157>
2013-04-02 20:55:19 +1100

    - Removed old menu system - Cleaned out bin and lib folder

--- TCS/src/com/ezztech/tcs/MainActivity.java
+++ TCS/src/com/ezztech/tcs/MainActivity.java
@@ -1,6 +1,5 @@
 package com.ezztech.tcs;
 
-import net.simonvt.menudrawer.MenuDrawer;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,20 +7,16 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 	
-	private MenuDrawer mDrawer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		mDrawer = MenuDrawer.attach(this);
-		mDrawer.setContentView(R.layout.activity_main);
-		mDrawer.setMenuView(R.layout.activity_main_menu);
 	}
 	
 	public void timeTableClick(View v){
 		Intent intent = new Intent(this, TimeTable.class);
 		startActivity(intent);
-		mDrawer.closeMenu();
+		//Close the menu and start the new activity
 	}
 }
