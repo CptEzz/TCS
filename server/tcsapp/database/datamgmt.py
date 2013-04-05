8542dd716aac53b92ef6b374656b06bd9a59c208
Lachlan Healey <lachlan.j.healey@gmail.com>
2013-04-06 03:49:06 +1000

    Addition of various comments, fixing of closing methods, to limit memory usage.

--- server/tcsapp/database/datamgmt.py
+++ server/tcsapp/database/datamgmt.py
@@ -2,36 +2,50 @@ import sqlite3
 import os
 from .. import security
 
-database = 'data.sqlite'
-sqlite3.connect(database)
+databaseDirectory = 'data.sqlite'
+# By default this creates the database in the current directory,
+# in this case ...\GitHub\TCS\server\tcsapp\database
 
 def getConnection():
-    return sqlite3.connect(database)
+    # Ensures every method call is run on a unique connection, which allows
+    # multithreading.
+    return sqlite3.connect(databaseDirectory)
 
 def init():
-    cur = getConnection().cursor()
+    # Initialises the system, only to be run once.
+    connection = getConnection()
+    cur = connection.cursor()
     cur.execute('create table student(ID, name, email, password, salt)')
     cur.execute('create table class(ID, code, name, teacher)')
     cur.execute('create table enrollment(studentID, classID, period, day, room)')
     cur.commit()
-    cur.close()
+    connection.close()
     print 'Initialised'
 
 def register(name, email, password):
-    cur = getConnection().cursor()
+    # Creation of a new user
+    connection = getConnection()
+    cur = connection.cursor()
     salt = os.urandom(30)
     cur.execute('insert into student values (?, ?, ?, ?, ?)', (ID, name, email, passwd(password, salt), salt))
     cur.commit()
-    cur.close()
+    connection.close()
     
 def createClass(code, name, teacher):
-    cur = getConnection().cursor()
+    # Creation of a new class
+    connection = getConnection()
+    cur = connection.cursor()
     cur.execute('insert into class values (?, ?, ?, ?, ?)', (ID, code, name, teacher))
     cur.commit()
-    cur.close()
+    connection.close()
 
 def enrol(studentID, classID, period, day, room):
-    cur = getConnection().cursor()
+    # Creation of a new enrollment
+    connection = getConnection()
+    cur = connection.cursor()
     cur.execute('insert into enrollment values (?, ?, ?, ?, ?)', (studentID, classID, period, day, room))
     cur.commit()
-    cur.close()
+    connection.close()
+
+"""Still yet to write methods for editing and/or ammending of Student, Class
+or Enrollments"""
