74bafae4c09b757357d0bb87fe3e3c6b3d7083fc
Benny Mackney <bmackneypublic@gmail.com>
2013-04-09 23:40:36 +1000

    added tcsapp.security.stdhash to standardise hashing mechanism. Also fixes previous commit which was missing code.

--- server/tcsapp/security/__init__.py
+++ server/tcsapp/security/__init__.py
@@ -2,6 +2,9 @@
 
 import hashlib
 
+def stdhash(data):
+    return hashlib.sha512(data).hexdigest()
+
 def passwd(data,salt):
     """Takes a string data and string salt and hashes it suitably for storing passwords"""
     return stdhash(salt+data)
