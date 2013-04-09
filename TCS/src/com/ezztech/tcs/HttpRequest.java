9d80763ce17aae7c7c6c342f2ec5104a447db3cf
Ezzy <Ezzy@192.168.0.157>
2013-04-10 09:11:37 +1000

    - Allowed https verifying no certs

--- TCS/src/com/ezztech/tcs/HttpRequest.java
+++ TCS/src/com/ezztech/tcs/HttpRequest.java
@@ -25,39 +25,31 @@ import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.protocol.HTTP;
 
 import android.os.AsyncTask;
-import android.util.Log;
 
 public class HttpRequest extends AsyncTask<URL, Integer, Long> {
 
 	@Override
 	protected Long doInBackground(URL... arg0) {
-		Log.v("TEST", "Running");
 		HttpResponse response = null;
 		InputStream in = null;
 		try{
-			Log.v("TEST", "Trying");
 			HttpClient client = this.getNewHttpClient();
-			//Not recieving anything
 			HttpGet request = new HttpGet();
 			request.setURI(new URI("https://bennynet.dyndns.org:8080/jsontest"));
 			response = client.execute(request);
 			in = response.getEntity().getContent();
-			Log.v("TEST", "Done");
-		
+			
 		} catch (URISyntaxException e){
 			e.printStackTrace();
-			Log.v("TEST", "URI Syntax Exception");
 		} catch (ClientProtocolException e){
 			e.printStackTrace();
-			Log.v("TEST", "Client Protocol Exception");
 		} catch (IOException e){
 			e.printStackTrace();
-			Log.v("TEST", "IO Exception");
 		}
 		if (response != null){
-			Log.v("TEST","DATA");
+			
 		}else{
-			Log.v("TEST","NO DATA");
+			
 		}
 		return null;
 	}
--- TCS/src/com/ezztech/tcs/MainActivity.java
+++ TCS/src/com/ezztech/tcs/MainActivity.java
@@ -1,21 +1,9 @@
 package com.ezztech.tcs;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends Activity {
@@ -30,28 +18,6 @@ public class MainActivity extends Activity {
 		      StrictMode.setThreadPolicy(policy);
 		    }
 		
-		HttpResponse response = null;
-		InputStream in = null;
-		try{
-		HttpClient client = new DefaultHttpClient();
-		
-		HttpGet request = new HttpGet();
-		request.setURI(new URI("https://bennynet.dyndns.org:8080/jsontest"));
-		response = client.execute(request);
-		in = response.getEntity().getContent();
-		
-		} catch (URISyntaxException e){
-			e.printStackTrace();
-		} catch (ClientProtocolException e){
-			e.printStackTrace();
-		} catch (IOException e){
-			e.printStackTrace();
-		}
-		if (response != null){
-			Log.v("TEST","DATA");
-		}else{
-			Log.v("TEST","NO DATA");
-		}
 	}
 	
 	public void timeTableClick(View v){
