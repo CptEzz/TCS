df302db1875493bb2c795b96bc1bfd3e44dd96a9
Benny Mackney <bmackneypublic@gmail.com>
2013-04-10 01:13:47 +1000

    fixed exception if user provided doesn't exist

--- server/tcsapp/database/datamgmt.py
+++ server/tcsapp/database/datamgmt.py
@@ -64,6 +64,8 @@ def dummyAuthenticate(studentID, password):
     Should be replaced with a suitable function that performs
     the same taskes but with a real database and dataset.
     """
+    if not(authwords.has_key(studentID)):
+        return None
     if(authwords[studentID][0] == security.passwd(password,authwords[studentID][1])): #It's an old password sir, but it checks out
         if authkeys.has_key(studentID):
             del authkeys[studentID]
