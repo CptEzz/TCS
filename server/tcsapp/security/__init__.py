ae41ddd9574fec8d6c85e4be42cb6eca1a2a7436
Benny Mackney <bmackneypublic@gmail.com>
2013-04-09 23:39:17 +1000

    rearranged tcsapp.security

--- server/tcsapp/security/__init__.py
+++ server/tcsapp/security/__init__.py
@@ -1,3 +1,7 @@
 """Handles security for the application"""
 
-from passwd import *
+import hashlib
+
+def passwd(data,salt):
+    """Takes a string data and string salt and hashes it suitably for storing passwords"""
+    return stdhash(salt+data)
